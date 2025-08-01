4c115d8e7587872f0a795631aa512c1fa04546f9
Gerardo Turin <gera_1993_06@hotmail.com>
2025-08-01 14:14:01 -0300

    03 - trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
-import { prisma } from "@/lib/db";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import Client from "./client";
+import { Suspense } from "react";
+
 
 const HomePage = async () => {
-  const users = await prisma.user.findMany();
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({
+    text: "GERAAAAA",
+  }));
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      {
-        JSON.stringify(users, null, 2)
-      }
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
 
